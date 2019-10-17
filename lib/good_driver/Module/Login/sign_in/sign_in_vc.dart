b32d9e6951a85fb4d623a55f81dd686133e7e35d
先至科技 <huangy@shidegroup.com>
2019-10-17 18:29:20 +0800

    no message

--- lib/good_driver/Module/Login/sign_in/sign_in_vc.dart
+++ lib/good_driver/Module/Login/sign_in/sign_in_vc.dart
@@ -1,3 +1,47 @@
-class SigninVC {
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
+class SignInVC extends StatefulWidget {
+  @override
+  SignInVCState createState() => SignInVCState();
+}
+
+class SignInVCState extends State<SignInVC> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: Container(
+        child: Image.asset('lib/images/3.0x/login_bg.png'),
+      ),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      title: Text('登录'),
+      ///居中，默认是自适应
+      centerTitle: true,
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.star),
+          onPressed: () {
+            print('星星 iconBtn');
+          },
+        ),
+        FlatButton(
+          child: Text(
+            '点击注册',
+            style: TextStyle(
+              fontSize: 16.0,
+              color: Colors.white,
+            ),
+          ),
+          onPressed: () {
+            print('跳去注册');
+          },
+        )
+      ],
+    );
+  }
 }
--- lib/main_test.dart
+++ lib/main_test.dart
@@ -2,8 +2,12 @@ import 'dart:convert';
 import 'dart:core';
 
 import 'package:flutter/material.dart';
+import 'package:hxh_flutter_kit/good_driver/Module/Login/sign_in/sign_in_vc.dart';
 import 'package:hxh_flutter_kit/good_driver/NetworkManager/commonModels/sdg_cooperator_model.dart';
 import 'package:hxh_flutter_kit/good_driver/NetworkManager/network_manager.dart';
+
+///业务类
+import 'good_driver/Module/Login/sign_in/sign_in_vc.dart';
 
 void main() {
   runApp(MyApp());
@@ -44,38 +48,19 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            RaisedButton(
+              child: Text('跳登录页'),
+              onPressed: (){
+                Navigator.of(context).push(MaterialPageRoute(builder: (context){
+                  return SignInVC();
+                }));
+              },
+            ),
             MaterialButton(
               color: Colors.blue,
               textColor: Colors.white,
               child: Text('测试get'),
-              onPressed: () {
-                this.setState(() {
-                  _str = '请求中';
-                });
-                Map param = {
-                  'username': '18513500000',
-                  'page': '1',
-                  'limit': '999',
-                };
-                SDGDataManager().getCooperation(param, (state, isSuccess, resp){
-                  if (isSuccess) {
-                    for (var i = 0; i < resp.length; ++i) {
-                      SDGCooperatorModel model = resp[i];
-                      this.setState((){
-                        _str = _str + '\n' + model.customerName;
-
-                      });
-                    }
-                  } else {
-                    this.setState((){
-                      _str = resp.toString();
-
-                    });
-                  }
-
-                });
-
-              },
+              onPressed: _testRequestAction,
             ),
             Text(
               _str,
@@ -84,5 +69,31 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
     );
+  }
+  _testRequestAction() {
+    this.setState(() {
+      _str = '请求中';
+    });
+    Map param = {
+      'username': '18513500000',
+      'page': '1',
+      'limit': '999',
+    };
+    SDGDataManager().getCooperation(param, (state, isSuccess, resp){
+      if (isSuccess) {
+        for (var i = 0; i < resp.length; ++i) {
+          SDGCooperatorModel model = resp[i];
+          this.setState((){
+            _str = _str + '\n' + model.customerName;
+
+          });
+        }
+      } else {
+        this.setState((){
+          _str = resp.toString();
+
+        });
+      }
+    });
   }
 }
